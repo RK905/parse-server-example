1ecc24707d6a062e7fd0161ed07148506c6a28b8
Rayen Kamta <mr.rayenk@gmail.com>
2016-03-16 14:31:24 -0400

    chan

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'partyrox',
-  masterKey: process.env.MASTER_KEY || 'September5', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:80/parse'  // Don't forget to change to https if needed
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
